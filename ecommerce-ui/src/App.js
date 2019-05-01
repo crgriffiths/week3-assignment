de40a7979e9b0d150789a3e1173bc7697c1a572f
Colin Griffiths <colinrossgriffiths@gmail.com>
2019-04-30 22:24:03 -0700

    adding state to App

--- ecommerce-ui/src/App.js
+++ ecommerce-ui/src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ListingGrid from './components/Listing/ListingGrid';
 import Header from './components/Header/Header';
 import './App.css';
+import listings from './airbnbs.json';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <ListingGrid/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listings: [],
+      cartListings: []
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ListingGrid/>
+      </div>
+    );
+  }
 }
 
 export default App;
